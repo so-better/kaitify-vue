ea203bb5bcde28c3d91bb1e713dde6790f61de51
lingkai1 <kailing@shein.com>
2024-11-04 18:16:49 +0800

    项目初始化

---
+++ README.md
@@ -0,0 +1,3 @@
+#### 基于@kaitify/core开发的vue富文本编辑器核心库
+
+> 具体使用方法请参阅：[@kaitify/vue](https://www.so-better.cn/kaitify-vue/)
---
+++ examples/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+import { Editor, KNode } from '../src'
+import { content } from "./content"
+
+const count = ref<number>(0)
+const editor = ref<Editor | null>(null)
+
+onMounted(async () => {
+
+})
+
+</script>
+<style lang="less">
+html {
+  height: 100%;
+}
+
+body {
+  height: 100%;
+  margin: 0;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  outline: none;
+}
+
+#app {
+  height: 100%;
+  overflow-y: auto;
+}
+</style>
