8f1190565ee2195cf9e3b1e4bd62b930897fbbaa
so-better <sobetter2024@163.com>
2024-11-20 18:03:48 +0800

    更新

--- src/locale/en-us.ts
+++ src/locale/en-us.ts
@@ -3,11 +3,20 @@ export default {
   远程地址: 'Remote Address',
   附件名称: 'Attachment Name',
   附件地址: 'Attachment Address',
+  附件: 'Attachment',
   插入: 'Insert',
+  更新: 'Update',
   默认颜色: 'Default Color',
   默认字体: 'Default Font',
   默认字号: 'Default Font Size',
+  默认行高: 'Default Line Height',
   正文: 'Paragraph',
   图片名称: 'Image Name',
-  图片地址: 'Image Address'
+  图片地址: 'Image Address',
+  图片: 'Image',
+  链接地址: 'Link Address',
+  新窗口打开: 'New Window',
+  链接文字: 'Link Text',
+  是否自动播放: 'AutoPlay',
+  视频地址: 'Video Address'
 }
