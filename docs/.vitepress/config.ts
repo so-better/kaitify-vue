2782c44a182d7a477554a40298dd47dced9270a8
so-better <sobetter2024@163.com>
2024-12-26 15:45:10 +0800

    更新

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -54,6 +54,7 @@ export default defineConfig({
       { text: '指南', link: '/guide/introduction', activeMatch: '/guide' },
       { text: '内置菜单', link: '/menus/introduction', activeMatch: '/menus' },
       { text: '演示', link: '/demo' },
+      { text: '反馈', link: 'https://www.so-better.cn/feedback', target: '_blank' },
       { text: '更新日志', link: '/changelog' }
     ],
     sidebar: {
